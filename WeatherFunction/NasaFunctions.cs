8f80df79581e8b432d295d7e0a1f41325a1aa781
Bob <bob@crowley.software>
2026-03-14 14:52:14 -0400

    branch response based on runtime environment

--- WeatherFunction/NasaFunctions.cs
+++ WeatherFunction/NasaFunctions.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System.Text.Json;
@@ -14,12 +15,13 @@ namespace WeatherFunction
     {
         private readonly ILogger<NasaFunctions> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHostEnvironment _environment;
 
-        public NasaFunctions(ILogger<NasaFunctions> logger, IHttpClientFactory httpClientFactory)
+        public NasaFunctions(ILogger<NasaFunctions> logger, IHttpClientFactory httpClientFactory, IHostEnvironment environment)
         {
             _logger = logger;
             _httpClientFactory = httpClientFactory;
-
+            _environment = environment;
         }
 
         [Function("apod")]
@@ -28,34 +30,35 @@ namespace WeatherFunction
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Apod), Summary = "Successful response", Description = "Returns the APOD object with date, title, explanation, and image URLs.")]
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
-            Apod apod = new Apod
+            if (_environment.IsDevelopment())
             {
-                date = "2026-03-14",
-                explanation = "This response is coming from my laptop",
-                hdurl = "https://fastly.picsum.photos/id/184/4288/2848.jpg?hmac=l0fKWzmWf6ISTPMEm1WjRdxn35sg6U3GwZLn5lvKhTI",
-                media_type = "image",
-                service_version = "v1",
-                title = "Dev Tunnels in action!",
-                url = "https://fastly.picsum.photos/id/184/4288/2848.jpg?hmac=l0fKWzmWf6ISTPMEm1WjRdxn35sg6U3GwZLn5lvKhTI"
-            };
+                var apod = new Apod
+                {
+                    date = "2026-03-14",
+                    explanation = "This response is coming from my laptop",
+                    hdurl = "https://fastly.picsum.photos/id/184/4288/2848.jpg?hmac=l0fKWzmWf6ISTPMEm1WjRdxn35sg6U3GwZLn5lvKhTI",
+                    media_type = "image",
+                    service_version = "v1",
+                    title = "Dev Tunnels in action!",
+                    url = "https://fastly.picsum.photos/id/184/4288/2848.jpg?hmac=l0fKWzmWf6ISTPMEm1WjRdxn35sg6U3GwZLn5lvKhTI"
+                };
 
-            return new OkObjectResult(apod);
+                return new OkObjectResult(apod);
+            }
 
-            //https://fastly.picsum.photos/id/184/4288/2848.jpg?hmac=l0fKWzmWf6ISTPMEm1WjRdxn35sg6U3GwZLn5lvKhTI
-            //_logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            //var apikey = Environment.GetEnvironmentVariable("NASA_API_KEY"); 
+            var apikey = Environment.GetEnvironmentVariable("NASA_API_KEY");
 
-            //var result = await _httpClientFactory.CreateClient().GetAsync($"https://api.nasa.gov/planetary/apod?api_key={apikey}");
+            var result = await _httpClientFactory.CreateClient().GetAsync($"https://api.nasa.gov/planetary/apod?api_key={apikey}");
 
-            //result.EnsureSuccessStatusCode();
+            result.EnsureSuccessStatusCode();
 
-            //var content = await result.Content.ReadAsStringAsync();
+            var content = await result.Content.ReadAsStringAsync();
 
-            //// Deserialize the JSON response with System.Text.Json
-            //var apod = JsonSerializer.Deserialize<Apod>(content);
+            var apodResponse = JsonSerializer.Deserialize<Apod>(content);
 
-            //return new OkObjectResult(apod);
+            return new OkObjectResult(apodResponse);
         }
     }
 
